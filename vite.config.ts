0e0247e7c2e60dc6749e20ced373c9557bb35649
jubayerhossaindev <jubayerhossainbook@gmail.com>
2026-03-17 12:08:52 +0600

    feat: add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/banglacalender/',
+
   plugins: [react(), tailwindcss()],
 
   resolve: {
