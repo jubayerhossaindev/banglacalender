a98445092b727fe0b9f0072575efd1f27b1aa417
jubayerhossaindev <jubayerhossainbook@gmail.com>
2026-03-17 12:31:45 +0600

    fix: remove .tsx extension from import

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => (
   <TooltipProvider>
     <Toaster />
     <Sonner />
-    <BrowserRouter>
+    <BrowserRouter basename="/banglacalender">
       <Routes>
         <Route path="/" element={<Index />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
